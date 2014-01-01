b9e264eb7597e7792647d6fde9831aed8524cb24
fredericomenezesmail <fredericomenezes.mail@gmail.com>
2014-01-01 17:04:13 -0200

    add static pages links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ SampleApp::Application.routes.draw do
   get "static_pages/about"
 
   root 'static_pages#home'
+
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
